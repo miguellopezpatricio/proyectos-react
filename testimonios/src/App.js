aacb80f1c2621c8a4368b6e5157c3f5c48f05aef
mlpatri <ml.patri@gmail.com>
2022-04-02 10:26:02 +0200

    Primer proyecto de prácticas de React finalizado

--- testimonios/src/App.js
+++ testimonios/src/App.js
@@ -4,9 +4,25 @@ import {Testimonio} from './componentes/Testimonio.js';
 function App() {
   return (
     <div className="App">
+            <h1>Testimonios de nuestros alumnos:</h1>
       <div className='contenedor-principal'>
-      <h1>Testimonios</h1>
-      <Testimonio/>
+
+      <Testimonio
+        nombre='Emma Bostian'
+        pais='Suecia'
+        imagen='1'
+        cargo='Ingeniera'
+        empresa='Spotify'
+        testimonio='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+       />
+      <Testimonio
+        nombre='Pepe Pérez'
+        pais='Francia'
+        imagen='2'
+        cargo='Ingeniero'
+        empresa='IBM'
+        testimonio='Chop chop Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+       />
       </div>
     </div>
   );
--- testimonios/src/componentes/Testimonio.js
+++ testimonios/src/componentes/Testimonio.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import '../estilos/Testimonio.css';
 
-export function Testimonio (){
+export function Testimonio (props){
   return (
     <div className='contenedor-testimonio'>
       <img className='imagen-testimonio'
-      src={require('../imagenes/persona1.jpeg')}
+      src={require(`../imagenes/persona${props.imagen}.jpeg`)}
       alt='foto persona'/>
 
       <div className='contenedor-texto-testimonio'>
-        <p className='nombre-testimonio'>Emma Bostian en Suecia</p>
-        <p className='cargo-testimonio'>Ingeniera de software</p>
-        <p className='texto-testimonio'>Lorem ipsum...</p>
+        <p className='nombre-testimonio'>{props.nombre} en {props.pais}</p>
+        <p className='cargo-testimonio'>{props.cargo} en {props.empresa}</p>
+        <p className='texto-testimonio'>{props.testimonio}</p>
       </div>
     
     </div>
